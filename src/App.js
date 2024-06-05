76d15ec6621069dce9eb7e89b9916a985814e1ba
Varun Krishnan V <varunkrishnan0001@gmail.com>
2024-06-05 11:25:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import DocumentScanner from './Components/DocumentScanner';
+import CameraTake from './Components/CameraTake';
 
 function App() {
   return (
     <>
-      <DocumentScanner src={"https://media.istockphoto.com/id/949409412/photo/newspaper-and-coffee-on-wooden-background.jpg?s=612x612&w=0&k=20&c=I7awJAIxvfUDrbHBZ_pO5_LYc10lelzsMGyvZLF1kxQ="}/>
+      <CameraTake />
     </>
   );
 }
--- src/Components/CameraTake.jsx
+++ src/Components/CameraTake.jsx
@@ -1,48 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
 function CameraTake(props) {
+    const [facingMode, setFacingMode] = useState(FACING_MODES.ENVIRONMENT);
+
     function handleTakePhoto(dataUri) {
-        // Do stuff with the photo...
-        console.log('takePhoto');
+        // Process the photo data URI
+        console.log('Photo taken:', dataUri);
     }
 
     function handleTakePhotoAnimationDone(dataUri) {
-        // Do stuff with the photo...
-        console.log('takePhoto');
+        // Process the photo data URI after the animation
+        console.log('Photo taken (animation done):', dataUri);
     }
 
     function handleCameraError(error) {
-        console.log('handleCameraError', error);
+        console.error('Camera error:', error);
     }
 
     function handleCameraStart(stream) {
-        console.log('handleCameraStart');
+        console.log('Camera started');
     }
 
     function handleCameraStop() {
-        console.log('handleCameraStop');
+        console.log('Camera stopped');
+    }
+
+    function toggleFacingMode() {
+        setFacingMode((prevMode) =>
+            prevMode === FACING_MODES.ENVIRONMENT ? FACING_MODES.USER : FACING_MODES.ENVIRONMENT
+        );
     }
 
     return (
-        <Camera
-            onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
-            onTakePhotoAnimationDone={(dataUri) => { handleTakePhotoAnimationDone(dataUri); }}
-            onCameraError={(error) => { handleCameraError(error); }}
-            idealFacingMode={FACING_MODES.ENVIRONMENT}
-            idealResolution={{ width: 640, height: 480 }}
-            imageType={IMAGE_TYPES.JPG}
-            imageCompression={0.97}
-            isMaxResolution={true}
-            isImageMirror={false}
-            isSilentMode={false}
-            isDisplayStartCameraError={true}
-            isFullscreen={false}
-            sizeFactor={1}
-            onCameraStart={(stream) => { handleCameraStart(stream); }}
-            onCameraStop={() => { handleCameraStop(); }}
-        />
+        <div>
+            <Camera
+                onTakePhoto={(dataUri) => { handleTakePhoto(dataUri); }}
+                onTakePhotoAnimationDone={(dataUri) => { handleTakePhotoAnimationDone(dataUri); }}
+                onCameraError={(error) => { handleCameraError(error); }}
+                idealFacingMode={facingMode}
+                idealResolution={{ width: 640, height: 480 }}
+                imageType={IMAGE_TYPES.JPG}
+                imageCompression={0.97}
+                isMaxResolution={true}
+                isImageMirror={false}
+                isSilentMode={false}
+                isDisplayStartCameraError={true}
+                isFullscreen={false}
+                sizeFactor={1}
+                onCameraStart={(stream) => { handleCameraStart(stream); }}
+                onCameraStop={() => { handleCameraStop(); }}
+            />
+            <button onClick={toggleFacingMode}>
+                Switch Camera
+            </button>
+        </div>
     );
 }
 
